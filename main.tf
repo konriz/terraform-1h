3fd6204d30801e4c78792b74fe7e479efe8a59db
konrad.po <konrad.po@masterborn.com>
2023-03-14 09:49:06 +0100

    sample repository initialisation

---
+++ main.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_version = "1.4.0"
+
+  required_providers {
+    aws = {
+     source = "hashicorp/aws"
+     version = "4.52.0"
+    }
+  }
+}
+
+provider "aws" {}
+
+data "aws_region" "current" {}
